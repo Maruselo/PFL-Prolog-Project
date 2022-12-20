a22155738530b0c2870600251d48a4ca664bd1f2
SergioCarvalhais02 <up202007544@up.pt>
2022-12-20 14:48:23 +0000

    menu

--- src/game.pl
+++ src/game.pl
@@ -8,6 +8,11 @@ initial_board(5, [blank, blank, tent2, tent2, tent2, tent2, blank, blank]).
 initial_board(6, [blank, blank, tent2, head2, head2, tent2, blank, blank]).
 initial_board(7, [blank, blank, tent2, head2, head2, tent2, blank, blank]).
 
+% menu_option(+OptionNumber, -Option)
+menu_option(1, play).
+menu_option(2, instructions).
+menu_option(3, quit).
+
 % initial_state(-GameState-Player)
 initial_state(GameState-Player) :- 
     initial_state(7, GameState),
@@ -63,3 +68,44 @@ play :-
     display_game(GameState-Player).
     %game_cycle(GameState-Player).
 
+instructions :-
+    write('Tako Judo - the timeless sport of octopus wrestling - is'), nl,
+    write('an abstract strategy game for 2 to 4 players in which the'), nl,
+    write('object is to prevent your opponent(s) from being able to'), nl,
+    write('move. Played on a 64-square board (for two players) or a'), nl,
+    write('100-square board (for three or four players), each player'), nl,
+    write('controls the head and eight tentacles of an octopus. The'), nl,
+    write('head takes up a 2x2 square area, and each tentacle takes'), nl,
+    write('up one square. Any piece can move like a Chess queen (any'), nl,
+    write('distance along an open orthogonal or diagonal line, but'), nl,
+    write('without jumping) provided that there is also a clear line'), nl,
+    write('of sight between that piece`s original location and the'), nl,
+    write('head piece for the octopus to which it belongs. A player'), nl,
+    write('whose octopus is no longer able to effectively move its'), nl,
+    write('head or any tentacles (either move them at all or only'), nl,
+    write('harmlessly move a tentacle back and forth between a couple'), nl,
+    write('of squares) is pinned and eliminated from the game. Last'), nl,
+    write('octopus to move freely controls the sea and wins the game.'), nl,
+    call(menu).
+
+quit :-
+    nl.
+
+menu :-
+    repeat,
+    nl,
+    write('Please select a number:'), nl,
+    write_menu_list,
+    read(OptionNumber),
+    (   menu_option(OptionNumber, OptionName)
+    ->  write('You selected: '), write(OptionName), nl, !
+    ;   write('Not a valid choice, try again...'), nl, fail
+    ),
+    nl,
+    call(OptionName).
+
+write_menu_list :-
+    menu_option(N, Name),
+    write(N), write('. '), write(Name), nl,
+    fail.
+write_menu_list.
